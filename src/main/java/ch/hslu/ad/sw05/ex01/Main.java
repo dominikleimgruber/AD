f0e1ceac5b36f30be4203e51c8adecaa978ac307
Dominik Leimgruber <db.leimgruber@bluewin.ch>
2020-03-31 20:27:32 +0200

    sw05 3rd commit

--- src/main/java/ch/hslu/ad/sw05/ex01/Main.java
+++ src/main/java/ch/hslu/ad/sw05/ex01/Main.java
@@ -1,25 +1,32 @@
 package ch.hslu.ad.sw05.ex01;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    private static final Logger LOG = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
 
 
         Random random = new Random();
-
         String[] colors = {"red", "black", "blue", "yellow", "green", "magenta"};
 
 
         List<Ball> balls = new ArrayList<>();
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
+
             int diameter = random.nextInt(31) + 20;
-            int xPosition = random.nextInt((601 - (diameter))) + (diameter);
+            int xPosition = random.nextInt(600 - (2 * diameter)) + diameter;
+            LOG.info(xPosition);
             int yPosition = random.nextInt(101) + (diameter / 2);
+
             Ball ball = new Ball(diameter, xPosition, yPosition, colors[random.nextInt(6)]);
             balls.add(ball);
             threads.add(new Thread(ball, "Ball" + i));
@@ -29,7 +36,11 @@ public class Main {
             thread.start();
         }
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                LOG.info(e);
+            }
         }
         System.exit(0);
     }
