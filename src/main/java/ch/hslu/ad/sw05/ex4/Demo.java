c0a1d18bd90b7cf9fb6ad3353941ab26c3d17275
Dominik Leimgruber <56133959+dominikleimgruber@users.noreply.github.com>
2020-04-02 17:04:36 +0200

    Add files via upload

---
+++ src/main/java/ch/hslu/ad/sw05/ex4/Demo.java
@@ -0,0 +1,20 @@
+package ch.hslu.ad.sw05.ex4;
+
+public class Demo {
+
+
+    public static void main(String[] args) {
+
+        Thread t3 = new Thread(new JoinAndSleep(null, 4000), "Thread3");
+        Thread t2 = new Thread(new JoinAndSleep(t3, 3000), "Thread2");
+        Thread t1 = new Thread(new JoinAndSleep(t2, 2000), "Thread1");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+
+    }
+
+
+}
---
+++ src/main/java/ch/hslu/ad/sw05/ex4/JoinAndSleep.java
@@ -0,0 +1,39 @@
+package ch.hslu.ad.sw05.ex4;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class JoinAndSleep implements Runnable {
+
+
+    private static final Logger LOG = LogManager.getLogger(JoinAndSleep.class);
+    private Thread t;
+    private int sleepTime;
+
+    public JoinAndSleep(final Thread t, final int sleepTime) {
+        this.t = t;
+        this.sleepTime = sleepTime;
+    }
+
+    @Override
+    public void run() {
+
+
+        LOG.info("Start task in Thread: {} ", Thread.currentThread().getName());
+        if(t != null){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+               LOG.info(e);
+            }
+        }
+
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            LOG.info(e);
+        }
+        LOG.info("End task in Thread: {} ", Thread.currentThread().getName());
+    }
+}
